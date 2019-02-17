d27c84944e167d26b0b06151191e26656fbd69d1
Laurits Aabye <laurits@sharkcell.dk>
2019-02-17 23:13:29 +0100

    commit

---
+++ app/src/main/java/com/production/outlau/codenamesti/activities/SoundsActivityTEST.java
@@ -0,0 +1,44 @@
+//package com.production.outlau.codenamesti.activities;
+//
+//import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.widget.ListView;
+//
+//import com.production.outlau.codenamesti.R;
+//import com.production.outlau.codenamesti.controllers.SoundListAdapter;
+//import com.production.outlau.codenamesti.models.SoundCard;
+//
+//import java.util.ArrayList;
+//
+//public class SoundsActivityTEST extends AppCompatActivity {
+//
+//    SoundListAdapter soundListAdapter;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_sounds);
+//
+//
+////        playAudio("https://upload.wikimedia.org/wikipedia/commons/6/6c/Grieg_Lyric_Pieces_Kobold.ogg");
+//
+//        final ListView list = findViewById(R.id.list);
+//        ArrayList<SoundCard> arrayList = new ArrayList<>();
+//        arrayList.add(new SoundCard("JAVA", "https://www.tutorialspoint.com/java/", "https://www.tutorialspoint.com/java/images/java-mini-logo.jpg"));
+//        arrayList.add(new SoundCard("Python", "https://www.tutorialspoint.com/python/", "https://www.tutorialspoint.com/python/images/python-mini.jpg"));
+//        arrayList.add(new SoundCard("Javascript", "https://www.tutorialspoint.com/javascript/", "https://www.tutorialspoint.com/javascript/images/javascript-mini-logo.jpg"));
+//        arrayList.add(new SoundCard("Cprogramming", "https://www.tutorialspoint.com/cprogramming/", "https://www.tutorialspoint.com/cprogramming/images/c-mini-logo.jpg"));
+//        arrayList.add(new SoundCard("Cplusplus", "https://www.tutorialspoint.com/cplusplus/", "https://www.tutorialspoint.com/cplusplus/images/cpp-mini-logo.jpg"));
+//        arrayList.add(new SoundCard("Android", "https://www.tutorialspoint.com/android/", "https://www.tutorialspoint.com/android/images/android-mini-logo.jpg"));
+//        soundListAdapter = new SoundListAdapter(this, arrayList);
+//        list.setAdapter(soundListAdapter);
+//
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        soundListAdapter.stopConnection();
+//    }
+//
+//}
--- app/src/main/java/com/production/outlau/codenamesti/controllers/SoundListAdapter.java
+++ app/src/main/java/com/production/outlau/codenamesti/controllers/SoundListAdapter.java
@@ -1,36 +1,31 @@
 package com.production.outlau.codenamesti.controllers;
 
-import android.app.Activity;
-import android.app.Service;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.database.DataSetObserver;
-import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.IBinder;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.production.outlau.codenamesti.R;
-import com.production.outlau.codenamesti.activities.SoundsActivity;
-import com.production.outlau.codenamesti.models.ImageCard;
 import com.production.outlau.codenamesti.models.SoundCard;
 import com.production.outlau.codenamesti.services.MediaPlayerService;
 import com.squareup.picasso.Picasso;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
-public class SoundListAdapter implements ListAdapter {
+public class SoundListAdapter extends RecyclerView.Adapter<SoundListAdapter.MyViewHolder> {
     public static final String Broadcast_PLAY_NEW_AUDIO = "com.production.outlau.codenamesti.PlayNewAudio";
 
     private MediaPlayerService player;
@@ -39,11 +34,25 @@ public class SoundListAdapter implements ListAdapter {
     //Binding this Client to the AudioPlayer Service
     private ServiceConnection serviceConnection;
 
-    private ArrayList<SoundCard> arrayList;
+    private ArrayList<SoundCard> soundCardList;
     private Context context;
-    public SoundListAdapter(Context context, ArrayList<SoundCard> arrayList) {
-        this.arrayList=arrayList;
-        this.context=context;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public Button button;
+        public TextView title;
+        public ImageView image;
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            button = view.findViewById(R.id.stop);
+            image = view.findViewById(R.id.img);
+        }
+    }
+
+
+    public SoundListAdapter(Context context, ArrayList<SoundCard> soundCardList) {
+        this.context = context;
+        this.soundCardList = soundCardList;
         this.serviceConnection = new ServiceConnection() {
             @Override
             public void onServiceConnected(ComponentName name, IBinder service) {
@@ -59,84 +68,65 @@ public class SoundListAdapter implements ListAdapter {
             }
         };
     }
-    @Override
-    public boolean areAllItemsEnabled() {
-        return false;
-    }
-    @Override
-    public boolean isEnabled(int position) {
-        return true;
-    }
-    @Override
-    public void registerDataSetObserver(DataSetObserver observer) {
-    }
-    @Override
-    public void unregisterDataSetObserver(DataSetObserver observer) {
-    }
-    @Override
-    public int getCount() {
-        return arrayList.size();
-    }
-    @Override
-    public Object getItem(int position) {
-        return position;
-    }
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-    @Override
-    public boolean hasStableIds() {
-        return false;
-    }
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        final SoundCard soundCard = arrayList.get(position);
-        if(convertView == null) {
-            LayoutInflater layoutInflater = LayoutInflater.from(context);
-            convertView = layoutInflater.inflate(R.layout.sound_list_item, null);
-            convertView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-            convertView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                }
-            });
-            final Button button = convertView.findViewById(R.id.stop);
-            final TextView title = convertView.findViewById(R.id.title);
-            ImageView image = convertView.findViewById(R.id.img);
-            title.setText(soundCard.title);
-            Picasso.with(context)
-                    .load(soundCard.image)
-                    .into(image);
 
-            image.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    playAudio(soundCard.url);
-                }
-            });
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_sound, parent, false);
 
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    stopAudio();
-                }
-            });
+        return new MyViewHolder(itemView);
+    }
 
-        }
-        return convertView;
-    }
     @Override
-    public int getItemViewType(int position) {
-        return position;
-    }
-    @Override
-    public int getViewTypeCount() {
-        return arrayList.size();
-    }
-    @Override
-    public boolean isEmpty() {
-        return false;
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        final SoundCard soundCard = soundCardList.get(position);
+
+
+        System.out.println("onbindview");
+        holder.title.setText(soundCard.title);
+        Picasso.with(context)
+                .load(soundCard.image)
+                .into(holder.image);
+
+        holder.image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playAudio(soundCard.url);
+            }
+        });
+
+        holder.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopAudio();
+            }
+        });
+
+
+//        holder.title.setText(soundCard.getName());
+//
+//        holder.thumbnail.setImageResource(soundCard.getThumbnail());
+
+//        int colorResId = soundCard.getColor();
+//
+//        holder.background.setBackgroundResource(colorResId);
+
+//        holder.background.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(context, soundCard.getNextActivity());
+//                context.startActivity(intent);
+//            }
+//        });
+        // loading mainCard cover using Glide library
+//        Glide.with(context).load(mainCard.getThumbnail()).into(holder.thumbnail);
+
+//        holder.overflow.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                showPopupMenu(holder.overflow);
+//            }
+//        });
     }
 
     private void playAudio(String media) {
@@ -150,11 +140,21 @@ public class SoundListAdapter implements ListAdapter {
         } else {
             //Service is active
             //Send media with BroadcastReceiver
-                        System.out.println("TEST2");
+            System.out.println("TEST2");
             Intent broadcastIntent = new Intent(Broadcast_PLAY_NEW_AUDIO);
             broadcastIntent.putExtra("url",media);
             this.context.sendBroadcast(broadcastIntent);
         }
+        player.onCompletion();
+        //TODO
+//        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
+//            @Override
+//            public void onCompletion(MediaPlayer mp) {
+//                performOnEnd();
+//            }
+//
+//        });
+//        })
     }
 
     private void stopAudio() {
@@ -169,5 +169,34 @@ public class SoundListAdapter implements ListAdapter {
         if(serviceBound) {
             this.context.unbindService(serviceConnection);
         }
+    }
+
+
+    /**
+     * Click listener for popup menu items
+     */
+    class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
+
+        public MyMenuItemClickListener() {
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.action_add_favourite:
+                    Toast.makeText(context, "Add to favourite", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.action_play_next:
+                    Toast.makeText(context, "Play next", Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return soundCardList.size();
     }
 }
