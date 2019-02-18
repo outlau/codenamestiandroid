3e9f4ef8b95707051ee8cab7321222402f90eb52
Laurits Aabye <laurits@sharkcell.dk>
2019-02-18 22:42:28 +0100

    Sound Activity done

--- app/src/main/java/com/production/outlau/codenamesti/controllers/SoundListAdapter.java
+++ app/src/main/java/com/production/outlau/codenamesti/controllers/SoundListAdapter.java
@@ -1,12 +1,15 @@
 package com.production.outlau.codenamesti.controllers;
 
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
-import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnCompletionListener;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -24,12 +27,16 @@ import com.production.outlau.codenamesti.services.MediaPlayerService;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class SoundListAdapter extends RecyclerView.Adapter<SoundListAdapter.MyViewHolder> {
     public static final String Broadcast_PLAY_NEW_AUDIO = "com.production.outlau.codenamesti.PlayNewAudio";
 
     private MediaPlayerService player;
     private boolean serviceBound = false;
+
+    AsyncTask asyncTask;
 
     //Binding this Client to the AudioPlayer Service
     private ServiceConnection serviceConnection;
@@ -38,17 +45,87 @@ public class SoundListAdapter extends RecyclerView.Adapter<SoundListAdapter.MyVi
     private Context context;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        public Button button;
         public TextView title;
         public ImageView image;
+        public boolean isPlaying = false;
+        public AsyncMediaTask asyncTask;
+        public volatile CountDownLatch mediaDoneLatch;
         public MyViewHolder(View view) {
             super(view);
-            title = (TextView) view.findViewById(R.id.title);
-            button = view.findViewById(R.id.stop);
+            title = view.findViewById(R.id.title);
             image = view.findViewById(R.id.img);
+            asyncTask = new AsyncMediaTask(this);
+            mediaDoneLatch = new CountDownLatch(1);
+            LocalBroadcastManager.getInstance(context).registerReceiver(completeMessage,
+                    new IntentFilter("completeMessage"));
+            LocalBroadcastManager.getInstance(context).registerReceiver(startMessage,
+                    new IntentFilter("startMessage"));
         }
-    }
 
+        public void resetLatch(){
+            isPlaying = false;
+            mediaDoneLatch.countDown();
+            mediaDoneLatch = new CountDownLatch(1);
+        }
+
+        public void playAudio(String media) {
+            resetLatch();
+
+            isPlaying = true;
+
+            asyncTask.cancel(true);
+            asyncTask = new AsyncMediaTask(this);
+            asyncTask.execute();
+
+            image.setImageDrawable(context.getResources().getDrawable(R.drawable.stop,null));
+
+            if (!serviceBound) {
+                Intent playerIntent = new Intent(context, MediaPlayerService.class);
+                playerIntent.putExtra("url", media);
+                playerIntent.putExtra("title", title.getText().toString());
+                context.startService(playerIntent);
+                context.bindService(playerIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+            } else {
+                //Service is active
+                //Send media with BroadcastReceiver
+                Intent broadcastIntent = new Intent(Broadcast_PLAY_NEW_AUDIO);
+                broadcastIntent.putExtra("url",media);
+                broadcastIntent.putExtra("title", title.getText().toString());
+                context.sendBroadcast(broadcastIntent);
+            }
+        }
+
+        public void stopAudio() {
+            resetLatch();
+            if (serviceBound) {
+                Intent broadcastIntent = new Intent(Broadcast_PLAY_NEW_AUDIO);
+                context.sendBroadcast(broadcastIntent);
+            }
+        }
+
+        private BroadcastReceiver completeMessage = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                // Get extra data included in the Intent
+                String message = intent.getStringExtra("message");
+                System.out.println("receiver " + "Got message: " + message);
+                resetLatch();
+            }
+        };
+
+        private BroadcastReceiver startMessage = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String message = intent.getStringExtra("message");
+                String titleText = intent.getStringExtra("title");
+                System.out.println("receiver " + "Got message titleText: " + titleText);
+                if(title.getText().toString().equals(titleText)){
+                    return;
+                }
+                resetLatch();
+            }
+        };
+    }
 
     public SoundListAdapter(Context context, ArrayList<SoundCard> soundCardList) {
         this.context = context;
@@ -80,119 +157,69 @@ public class SoundListAdapter extends RecyclerView.Adapter<SoundListAdapter.MyVi
     @Override
     public void onBindViewHolder(final MyViewHolder holder, int position) {
         final SoundCard soundCard = soundCardList.get(position);
-
-
-        System.out.println("onbindview");
         holder.title.setText(soundCard.title);
-        Picasso.with(context)
-                .load(soundCard.image)
-                .into(holder.image);
+//        Picasso.with(context)
+//                .load(soundCard.image)
+//                .into(holder.image);
 
         holder.image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                playAudio(soundCard.url);
+                if(holder.isPlaying){
+                    holder.stopAudio();
+                } else {
+                    holder.playAudio(soundCard.url);
+                }
             }
         });
-
-        holder.button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopAudio();
-            }
-        });
-
-
-//        holder.title.setText(soundCard.getName());
-//
-//        holder.thumbnail.setImageResource(soundCard.getThumbnail());
-
-//        int colorResId = soundCard.getColor();
-//
-//        holder.background.setBackgroundResource(colorResId);
-
-//        holder.background.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(context, soundCard.getNextActivity());
-//                context.startActivity(intent);
-//            }
-//        });
-        // loading mainCard cover using Glide library
-//        Glide.with(context).load(mainCard.getThumbnail()).into(holder.thumbnail);
-
-//        holder.overflow.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                showPopupMenu(holder.overflow);
-//            }
-//        });
-    }
-
-    private void playAudio(String media) {
-        //Check is service is active
-        if (!serviceBound) {
-            System.out.println("TEST");
-            Intent playerIntent = new Intent(this.context, MediaPlayerService.class);
-            playerIntent.putExtra("media", media);
-            this.context.startService(playerIntent);
-            this.context.bindService(playerIntent, serviceConnection, Context.BIND_AUTO_CREATE);
-        } else {
-            //Service is active
-            //Send media with BroadcastReceiver
-            System.out.println("TEST2");
-            Intent broadcastIntent = new Intent(Broadcast_PLAY_NEW_AUDIO);
-            broadcastIntent.putExtra("url",media);
-            this.context.sendBroadcast(broadcastIntent);
-        }
-        player.onCompletion();
-        //TODO
-//        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
-//            @Override
-//            public void onCompletion(MediaPlayer mp) {
-//                performOnEnd();
-//            }
-//
-//        });
-//        })
-    }
-
-    private void stopAudio() {
-        if (serviceBound) {
-            Intent broadcastIntent = new Intent(Broadcast_PLAY_NEW_AUDIO);
-            this.context.sendBroadcast(broadcastIntent);
-        }
-    }
-
-    public void stopConnection() {
-        stopAudio();
-        if(serviceBound) {
-            this.context.unbindService(serviceConnection);
-        }
     }
 
 
-    /**
-     * Click listener for popup menu items
-     */
-    class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
 
-        public MyMenuItemClickListener() {
+    // TODO
+//    public void stopConnection() {
+//        stopAudio();
+//        if(serviceBound) {
+//            this.context.unbindService(serviceConnection);
+//        }
+//        LocalBroadcastManager.getInstance(this.context).unregisterReceiver(mMessageReceiver);
+//    }
+
+    private class AsyncMediaTask extends AsyncTask<Object, Boolean, Boolean> {
+        MyViewHolder holder;
+
+        AsyncMediaTask(MyViewHolder holder){
+            this.holder = holder;
         }
 
         @Override
-        public boolean onMenuItemClick(MenuItem menuItem) {
-            switch (menuItem.getItemId()) {
-                case R.id.action_add_favourite:
-                    Toast.makeText(context, "Add to favourite", Toast.LENGTH_SHORT).show();
-                    return true;
-                case R.id.action_play_next:
-                    Toast.makeText(context, "Play next", Toast.LENGTH_SHORT).show();
-                    return true;
-                default:
+        protected Boolean doInBackground(Object... params) {
+            try {
+                return holder.mediaDoneLatch.await(60, TimeUnit.SECONDS);
+            } catch (final InterruptedException e) {
+                System.out.println("Someone has disturbed the condition awaiter.");
+                return false;
             }
-            return false;
         }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            updateUI(holder);
+            holder.resetLatch();
+        }
+
+    }
+
+    void updateUI(final MyViewHolder holder){
+        Handler mainHandler = new Handler(context.getMainLooper());
+
+        Runnable myRunnable = new Runnable() {
+            @Override
+            public void run() {
+                holder.image.setImageDrawable(context.getResources().getDrawable(R.drawable.play,null));
+            }
+        };
+        mainHandler.post(myRunnable);
     }
 
     @Override
